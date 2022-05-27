9bc1631afdd7cdf4eab19f0899303c2cba39774c
bidhandev <bidhandev.d@gmail.com>
2022-05-27 13:33:28 +0600

    Add laravel-mix and eslint

---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+/* eslint-disable indent */
+const mix = require('laravel-mix')
+
+mix
+    .js('resources/js/app.js', 'public/js/app.js')
+    .sass('resources/scss/app.scss', 'public/css/app.css')
